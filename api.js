ddf24a222f7bf7cb50a3e311e3dc796f9026700b
Bruno <bruno18042005@gmail.com>
2025-04-28 21:58:40 -0300

    commit 3

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-import express from 'express';  // Note as aspas ao redor de 'express'
+import express from 'express';
 import cors from 'cors';
 
 const app = express();
@@ -6,13 +6,13 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res,json({
-        date: new Date().toLocaleString(pt-BR),
+    res.json({  
+        date: new Date().toLocaleString('pt-BR'), 
         status: 'API no Render funcionando!'
     });
 });
 
-//porta dinamica p render
+// Porta dinâmica para o Render
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
